8df1e3b173c8cf920b4291aad187d7e7df19fd0f
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-24 16:54:01 -0300

    add color property to struct image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -15,6 +15,6 @@ defmodule Identicon do
   def pick_color(image) do
     %Identicon.Image{hex: [r, g, b | _tail]} = image
     
-    [r, g, b]
+    %Identicon.Image{image | color: {r,g,b}}
   end
 end
