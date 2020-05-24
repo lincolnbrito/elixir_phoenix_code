6ad4dade5fad13a5717212f4875e4b423c0475f4
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-24 00:03:06 -0300

    load deck from file

--- lib/elixir_cards.ex
+++ lib/elixir_cards.ex
@@ -24,4 +24,9 @@ defmodule ElixirCards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    {status binary} = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
 end
