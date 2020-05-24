b07ea1b15ca5a5f8e9eba202ee5808612bfd3eb2
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-23 22:56:30 -0300

    create deck using nested comprehensions

--- lib/elixir_cards.ex
+++ lib/elixir_cards.ex
@@ -1,6 +1,13 @@
 defmodule ElixirCards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   def shuffle(deck) do
