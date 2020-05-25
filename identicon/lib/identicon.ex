a7adc5ad093753587ed1aa57da95f79925bcb807
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-25 14:03:05 -0300

    chunck hex list

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -14,5 +15,10 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do    
     %Identicon.Image{image | color: {r,g,b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
   end
 end
