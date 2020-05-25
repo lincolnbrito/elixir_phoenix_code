ee66e37eedf6a903c725b759da280583c6103f9e
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-25 20:39:17 -0300

    create helper function to mirror row

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -13,12 +13,18 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do    
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r,g,b}}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail ] = row
+
+    row ++ [second, first]
   end
 end
