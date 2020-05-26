c9bb45a5b45b8d4622eac2323236a8950f19803f
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-05-25 22:14:13 -0300

    filter odd squares

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
@@ -24,6 +25,14 @@ defmodule Identicon do
       |> Enum.map(&mirror_row/1)
       |> List.flatten
       |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) -> 
+      rem(code, 2) == 0
+    end
 
     %Identicon.Image{image | grid: grid}
   end
